a7d5eb717e8a67b89bfc88a7219144489610b3fa
Jardiano Rei Delas <jardiano.almeida@gmail.com>
2022-01-29 11:16:05 -0300

    AnimatedBuilder

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,41 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
-final _counter = Counter();
-
 class HomePage extends HookWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final controller = useListenable(_counter);
-    final textController = useTextEditingController();
+    final counter = useValueNotifier(0);
 
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: [
-          Center(
-            child: Text('${controller.value}'),
-          ),
-          TextField(
-            controller: textController,
-          )
-        ],
+      body: Center(
+        child: AnimatedBuilder(
+          animation: counter,
+          builder: (context, snapshot) {
+            return Text('${counter.value}');
+          }
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          controller.increment();
+          counter.value++;
         },
       ),
     );
-  }
-}
-
-class Counter extends ChangeNotifier {
-  int value = 0;
-  increment() {
-    value++;
-    notifyListeners();
   }
 }
